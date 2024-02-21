4431665f0aed371f54085ab95b7a6fad6d834d21
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2024-02-21 16:03:05 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,4 @@
 const { Sequelize } = require('sequelize')
-const dbName = require("./config.json").Database;
-const dbUser = require("./config.json").Username;
-const dbPwd = require("./config.json").Password;
-const dbHost = require("./config.json").Hostname;
 // database
 const sequelize = new Sequelize(
   process.env.DB_NAME, // Database name
